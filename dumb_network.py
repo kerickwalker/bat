d51a16dc4516ee238cc5aadd6de87cbea1145d9a
Kerick Walker <kerickwalker@gmail.com>
2023-11-29 00:39:29 +0100

    Move network into an ipy notebook and cells to load the data and train the network

--- dumb_network.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras import models, layers
-
-dumb_network = models.Sequential()
-dumb_network.add(layers.Dense(100, activation='relu', input_shape=(400, 100, 3)))
-dumb_network.add(layers.Dense(20, activation='relu'))
-dumb_network.add(layers.Dense(2, activation='relu'))
-dumb_network.summary()
